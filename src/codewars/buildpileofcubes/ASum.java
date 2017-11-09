86af60de53f862a5678e5744800915203baf2c48
malco <malco@192.168.100.27>
2017-11-09 06:51:47 +0000

    Updated to use function to calc sum of cubes

--- src/codewars/buildpileofcubes/ASum.java
+++ src/codewars/buildpileofcubes/ASum.java
@@ -14,83 +14,82 @@ The parameter of the function findNb (find_nb, find-nb, findNb) will
 */
 
 public class ASum {
-	public static long findNb(long m) {
+    public static long findNb(long m) {
 
-		long buildingVolume = m;
+        long buildingVolume = m;
 
-		// iterative solution to find num of cubes for the volume.
-		// start low, go up in 100s and then tune the increment
-		// to zero in on the number.
-		int numCubes = 50;
-		int increment = 100;
-		boolean numCubesFound = false;
-		while (true) {
-			long calculatedVolume = calcVolume(numCubes);
-			System.out.println(String.format("numCubes %d calculatedVol %d targetVol %d lastIncrement %d", numCubes,
-					calculatedVolume, buildingVolume, increment));
+        // iterative solution to find num of cubes for the volume.
+        // start low, go up in 100s and then tune the increment
+        // to zero in on the number.
+        int numCubes = 50;
+        int increment = 100;
+        boolean numCubesFound = false;
+        while (true) {
+            long calculatedVolume = (long) (Math.pow(numCubes, 2) * Math.pow(numCubes + 1, 2) / 4);
+            System.out.println(String.format("numCubes %d calculatedVol %d targetVol %d lastIncrement %d", numCubes,
+                    calculatedVolume, buildingVolume, increment));
 
-			if (calculatedVolume == buildingVolume) {
-				numCubesFound = true;
-				break;
-			} else if (((calculatedVolume > buildingVolume) && (increment == 1))
-					|| ((calculatedVolume < buildingVolume) && (increment == -1))) {
-				// if the number is 'unreachable' will end up oscillating
-				// around 1/-1 increments, so prevent
-				numCubesFound = false;
-				break;
-			} else {
-				increment = getNextIncrement(buildingVolume, calculatedVolume, increment);
-				numCubes += increment;
-			}
-		}
-		if (numCubesFound) {
-			return numCubes;
-		} else {
-			return -1;
-		}
+            if (calculatedVolume == buildingVolume) {
+                numCubesFound = true;
+                break;
+            } else if (((calculatedVolume > buildingVolume) && (increment == 1))
+                    || ((calculatedVolume < buildingVolume) && (increment == -1))) {
+                // if the number is 'unreachable' will end up oscillating
+                // around 1/-1 increments, so prevent
+                numCubesFound = false;
+                break;
+            } else {
+                increment = getNextIncrement(buildingVolume, calculatedVolume, increment);
+                numCubes += increment;
+            }
+        }
+        if (numCubesFound) {
+            return numCubes;
+        } else {
+            return -1;
+        }
 
-	}
+    }
 
-	public static int getNextIncrement(long targetVol, long calcVol, int lastIncrement) {
-		// go up in 100s by default. if overshoot, go back by 50, 25 etc.
-		int nextIncrement;
-		if (calcVol > targetVol) {
-			if (lastIncrement > 1) {
-				nextIncrement = -1 * lastIncrement / 2;
-			} else {
-				// lastIncrement already negative.. keep going
-				nextIncrement = lastIncrement;
-			}
-		} else {
-			// calculated volume is less than target volume
-			// keep going in same direction if were incrementing
-			if (lastIncrement >= 1) {
-				nextIncrement = lastIncrement;
-			} else {
-				// we were coming backwards, but went too far
-				nextIncrement = -1 * lastIncrement / 2;
-			}
-		}
-		return nextIncrement;
-	}
+    public static int getNextIncrement(long targetNum, long currentNum, int lastIncrement) {
+        // go up in 100s by default. if overshoot, go back by 50, 25 etc.
+        int nextIncrement;
+        if (currentNum > targetNum) {
+            if (lastIncrement > 1) {
+                nextIncrement = -1 * lastIncrement / 2;
+            } else {
+                // lastIncrement already negative.. keep going
+                nextIncrement = lastIncrement;
+            }
+        } else {
+            // calculated volume is less than target volume
+            // keep going in same direction if were incrementing
+            if (lastIncrement >= 1) {
+                nextIncrement = lastIncrement;
+            } else {
+                // we were coming backwards, but went too far
+                nextIncrement = -1 * lastIncrement / 2;
+            }
+        }
+        return nextIncrement;
+    }
 
-	public static long calcVolume(int numCubes) {
-		// initially just doing the easy way to picture the problem
-		// so for n cubes, the bottom one is n^3, until top 1 is 1^3
-		// so if 3 cubes, would be 3^3, 2^3, 1^3
+    // not used now - kept here for posterity!
+    public static long calcVolume(int numCubes) {
+        // initially just doing the easy way to picture the problem
+        // so for n cubes, the bottom one is n^3, until top 1 is 1^3
+        // so if 3 cubes, would be 3^3, 2^3, 1^3
 
-		System.out.println(String.format("CalcVolume for %d cubes", numCubes));
-		long buildingVolume = 0;
-		/*
-		for (int i = 1; i <= numCubes; i++) {
-			long blockVolume = (long) Math.pow(i, 3);
-			buildingVolume += blockVolume;
-			// System.out.println(String.format("Block %d Volume %d total volume %d", i,
-			// blockVolume, buildingVolume));
-		}
-*/
-		buildingVolume =(long) (Math.pow(numCubes, 2) * Math.pow(numCubes+1, 2)/4);
-		
-		return buildingVolume;
-	}
+        System.out.println(String.format("CalcVolume for %d cubes", numCubes));
+        long buildingVolume = 0;
+        /*
+         * for (int i = 1; i <= numCubes; i++) { long blockVolume = (long)
+         * Math.pow(i, 3); buildingVolume += blockVolume; //
+         * System.out.println(String.format("Block %d Volume %d total volume %d"
+         * , i, // blockVolume, buildingVolume)); }
+         */
+        buildingVolume = (long) (Math.pow(numCubes, 2) * Math.pow(numCubes + 1, 2) / 4);
+
+        return buildingVolume;
+    }
 }
--- src/codewars/buildpileofcubes/ASumTest.java
+++ src/codewars/buildpileofcubes/ASumTest.java
@@ -6,40 +6,49 @@ import org.junit.Test;
 
 public class ASumTest {
 
-	@Test
-	public void testCalcVolume() {
-		assertEquals(1071225, ASum.calcVolume(45));
+    @Test
+    public void testCalcVolume() {
+        assertEquals(1071225, ASum.calcVolume(45));
 
-	}
+    }
 
-	@Test
-	public void test1() throws Exception {
-		assertEquals(2022, ASum.findNb(4183059834009L));
-	}
+    @Test
+    public void test1() throws Exception {
+        assertEquals(2022, ASum.findNb(4183059834009L));
+    }
 
-	//@Test
-	//public void test2() {
-	//	assertEquals(-1, ASum.findNb(24723578342962L));
-	//}
+     @Test
+     public void test2() {
+     assertEquals(-1, ASum.findNb(24723578342962L));
+     }
 
-	@Test
-	public void test3() throws Exception {
-		//initial timing 1.5seconds for this
-		
-		assertEquals(4824, ASum.findNb(135440716410000L));
-	}
+    @Test
+    public void test3() throws Exception {
+        // initial timing 1.5seconds for this
 
-	@Test
-	public void test4() throws Exception {
-		assertEquals(3568, ASum.findNb(40539911473216L));
-	}
+        assertEquals(4824, ASum.findNb(135440716410000L));
+    }
 
-	@Test
-	public void testGetNextIncement() throws Exception{
-		assertEquals(100, ASum.getNextIncrement(1000, 900, 100));
-		assertEquals(-50, ASum.getNextIncrement(1000,  1100, 100));
-		assertEquals(25, ASum.getNextIncrement(1000, 900, -50));
-		
-	}
-	
+    @Test
+    public void test4() throws Exception {
+        assertEquals(3568, ASum.findNb(40539911473216L));
+    }
+
+/*    @Test
+    public void test5() {
+        assertEquals(-1, ASum.findNb(2304422822859502500));
+    }
+    @Test
+    public void test6() {
+        assertEquals(-1, ASum.findNb(2160060523386798096));
+    }
+*/
+    @Test
+    public void testGetNextIncement() throws Exception {
+        assertEquals(100, ASum.getNextIncrement(1000, 900, 100));
+        assertEquals(-50, ASum.getNextIncrement(1000, 1100, 100));
+        assertEquals(25, ASum.getNextIncrement(1000, 900, -50));
+
+    }
+
 }
